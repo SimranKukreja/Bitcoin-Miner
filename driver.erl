55e11c37de92cc932f4fa581a1ee77f9c82665ef
SimranKukreja <skukreja25@gmail.com>
2023-06-07 21:05:06 -0400

    Delete driver.erl

--- driver.erl
+++
@@ -1,8 +0,0 @@
--module(driver).
--compile(export_all).
-
-startServer(NoOfZeros) ->
-    register(server, spawn(server, start, [NoOfZeros])).
-
-startClient(ServerAddress) ->
-    register(client, spawn(client, start, [ServerAddress])).
